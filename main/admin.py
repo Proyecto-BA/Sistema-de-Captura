dbc459733e0bc77ca2478085c7ebeefccd0e803a
OrlandoLR <99378874+OrlandoLR@users.noreply.github.com>
2024-05-28 18:44:56 -0700

    commit 3

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Covid
+admin.site.register(Covid)
+
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-class CountryWiseLatest(models.Model):
+class Covid(models.Model):
     country_region = models.TextField(db_column='Country/Region', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
     confirmed = models.IntegerField(db_column='Confirmed', blank=True, null=True)  # Field name made lowercase.
     deaths = models.IntegerField(db_column='Deaths', blank=True, null=True)  # Field name made lowercase.
@@ -21,11 +21,10 @@ class CountryWiseLatest(models.Model):
     deaths_100_recovered = models.FloatField(db_column='Deaths / 100 Recovered', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
     confirmed_last_week = models.IntegerField(db_column='Confirmed last week', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
     number_1_week_change = models.IntegerField(db_column='1 week change', blank=True, null=True)  # Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
-    number_1_week_increase = models.FloatField(db_column='1 week % increase', blank=True, null=True)  # Field renamed to remove unsuitable characters. Field renamed because it wasn't a valid Python identifier.
     who_region = models.TextField(db_column='WHO Region', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
 
     id = models.AutoField(primary_key=True)
-    
+
     class Meta:
         managed = False
         db_table = 'country_wise_latest'
